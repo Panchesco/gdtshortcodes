cf7b0a98739f619c4dc3ad51ffe8998a9d0c1744
Richard Whitmer <richard@panchesco.com>
2016-03-09 11:55:21 -0700

    Cleaned up some formatting

--- ext.gdtshortcodes.php
+++ ext.gdtshortcodes.php
@@ -508,12 +508,12 @@ public function disable_extension()
 		private function curl_get($url) 
 		{
 			$curl = curl_init($url);
-    	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    	curl_setopt($curl, CURLOPT_TIMEOUT, 30);
-    	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-    	$return = curl_exec($curl);
-    	curl_close($curl);
-    	return $return;
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+			curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+			$return = curl_exec($curl);
+			curl_close($curl);
+			return $return;
 		}
 		
 		//-----------------------------------------------------------------------------
